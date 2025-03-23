66190ff27fa4f9c2b90922e92a0aa56ff2424d51
w3schools-test <prabugce062@gmail.com>
2025-03-23 11:13:41 +0530

    init file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing code using deepcode AI.
         </a>
       </header>
     </div>
